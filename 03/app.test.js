e44d8b86a24120c736ea4ab67bb5831cfe120a55
TetraMeister <lukasz3s@wp.pl>
2026-03-22 19:34:29 +0100

    test 2 added

--- 03/app.test.js
+++ 03/app.test.js
@@ -5,9 +5,9 @@ describe('randomNumber(min, max)', () => {
   it('returns 1 if min and max properties are set to 1', () => {
     expect(randomNumber(1, 1)).toBe(1)
   })
-  // it('throws an error if min property is not a number', () => {
-  //   expect(randomNumber('1', 1))
-  // })
+  it('throws an error if min property is not a number', () => {
+    expect(randomNumber('1', 1))
+  })
   // it('throws an error if max property is not a number', () => {
   //   expect(randomNumber(1, '1'))
   // })
