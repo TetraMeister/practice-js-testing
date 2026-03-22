016d578577cd90bb7b1063b05b32e040af26f8db
TetraMeister <lukasz3s@wp.pl>
2026-03-22 20:12:45 +0100

    task04 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,33 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!email.includes('@')) {
+      throw new Error('Email validation failed!')
+    }
 
+    if (password.length <= 3) {
+      throw new Error('Hasło jest zbyt krótkie!')
+    }
+
+    if (!/(?=.*[0-9])(?=.*[a-zA-Z])/.test(password)) {
+      throw new Error('Hasło musi posiadać litery!')
+    }
+
+    this.email = email;
+    this.password = password;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    if (this.email.includes('devmentor.pl')) {
+      return true;
+    }
+    return false;
+  }
 }
