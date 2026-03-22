e327f6d353a5d018787afa0e805523c4ac972d58
TetraMeister <lukasz3s@wp.pl>
2026-03-22 19:56:26 +0100

    task03 refactored and done

--- 03/app.js
+++ 03/app.js
@@ -2,14 +2,8 @@ export default function randomNumber(min, max) {
   if (typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('Zły typ danych w funkcji randomNumber()')
   }
-
   if (min > max) {
     throw new Error('Źle ustawiony zakres funkcji randomNumber()')
   }
-
-  if (min === 1 && max === 1) {
-    return 1;
-  }
-
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
